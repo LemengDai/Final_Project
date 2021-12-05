f5a6729f384cded4f613f3b087c2930a7404223e
LemengDai <51035918+LemengDai@users.noreply.github.com>
2021-12-04 21:23:50 -0500

    Update main.py

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ Copyright and Usage Information
 ===============================
 
 This file is provided solely for the personal and private use of Lemeng Dai, Arthur Iliescu,
-Jiaxin Li, Maisarah Zulkefli. Arthur All forms of distribution of this code,
-whether as given or with any changes, are expressly prohibited.
+Jiaxin Li, Maisarah Zulkefli. All forms of distribution of this code, whether as given or 
+with any changes, are expressly prohibited.
 
 This file is Copyright (c) 2021 Lemeng Dai, Arthur Iliescu, Jiaxin Li, Maisarah Zulkefli.
 """

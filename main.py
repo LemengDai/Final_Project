5362f0bd083a5eebf0834103af93866a312f33d7
LemengDai <51035918+LemengDai@users.noreply.github.com>
2021-12-04 21:22:54 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+"""CSC110 Fall 2020: Final Project
+
+Copyright and Usage Information
+===============================
+
+This file is provided solely for the personal and private use of Lemeng Dai, Arthur Iliescu,
+Jiaxin Li, Maisarah Zulkefli. Arthur All forms of distribution of this code,
+whether as given or with any changes, are expressly prohibited.
+
+This file is Copyright (c) 2021 Lemeng Dai, Arthur Iliescu, Jiaxin Li, Maisarah Zulkefli.
+"""

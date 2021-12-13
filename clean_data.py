e5eeb0719f9bef4e2e746bff87b269ee0bc60bb0
LemengDai <51035918+LemengDai@users.noreply.github.com>
2021-12-13 17:11:49 -0500

    Update clean_data.py

--- clean_data.py
+++ clean_data.py
@@ -24,7 +24,7 @@ class CasesData:
         - number_confirmed: 12
         - number_total: 20
         - number_today: int
-        
+
     Representation Invariants:
         - province_id in [35, 59, 24, 48, 43, 17, 61, 62, 12, 60, 99, 46, 47, 11]
         - province_name in ['Alberta', 'Ontario', 'Quebec', 'British Columbia', 'Manitoba', \
@@ -41,14 +41,15 @@ class CasesData:
     number_today: int
 
 
-def return_data(filename: str):
+def return_data(filename: str) -> dict[str, dict[tuple[int, int], int]]:
     """A function to run and return the data for the filename.
     """
-    lst_provinces = ['Ontario', 'British Columbia', 'Alberta', 'Manitoba', 'Saskatchewan', 'Prince Edward Island',
-                     'New Brunswick', 'Nova Scotia', 'Quebec', 'Newfoundland and Labrador']
+    lst_provinces = ['Ontario', 'British Columbia', 'Alberta', 'Manitoba', 'Saskatchewan',
+                     'Prince Edward Island', 'New Brunswick', 'Nova Scotia', 'Quebec',
+                     'Newfoundland and Labrador']
 
     lst_years = [2020, 2021]
-    lst_months = [i for i in range(1, 13)]
+    lst_months = list(range(1, 13))
     inputs = load_data(filename)
     data = total_cases_per_years(inputs, lst_months, lst_years, lst_provinces)
     return data
@@ -83,8 +84,9 @@ def cases_per_month(inputs: list[CasesData], month: int, year: int, province: st
     Precondition:
         - month in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
         - year in [2020, 2021]
-        - province in ['Ontario', 'British Columbia', 'Alberta', 'Manitoba', 'Saskatchewan', 'Prince Edward Island',
-                     'New Brunswick', 'Nova Scotia', 'Quebec', 'Newfoundland and Labrador']
+        - province in ['Ontario', 'British Columbia', 'Alberta', 'Manitoba', 'Saskatchewan',
+        'Prince Edward Island', 'New Brunswick', 'Nova Scotia', 'Quebec',
+        'Newfoundland and Labrador']
     >>> method = [CasesData(province_id=35, province_name='Ontario', \
     date=datetime.date(2020, 1, 31), number_confirmed=3, number_total=3, number_today=3)]
     >>> cases_per_month(method, 1, 2020, 'Ontario')
@@ -100,13 +102,15 @@ def cases_per_month(inputs: list[CasesData], month: int, year: int, province: st
 
 def cases_per_month_province(inputs: list[CasesData], months: list[int], years: list[int],
                              province: str) -> dict[tuple[int, int], int]:
-    """Return a dictionary matching the month in a calendar year to the number of cases in that month for province.
-    
+    """Return a dictionary matching the month in a calendar year to the number of cases in that
+     month for province.
+
     Precondition:
         - month in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
         - year in [2020, 2021]
-        - province in ['Ontario', 'British Columbia', 'Alberta', 'Manitoba', 'Saskatchewan', 'Prince Edward Island',
-                     'New Brunswick', 'Nova Scotia', 'Quebec', 'Newfoundland and Labrador']
+        - province in ['Ontario', 'British Columbia', 'Alberta', 'Manitoba', 'Saskatchewan',
+        'Prince Edward Island', 'New Brunswick', 'Nova Scotia', 'Quebec',
+        'Newfoundland and Labrador']
     >>> method = [CasesData(province_id=35, province_name='Ontario', \
     date=datetime.date(2020, 1, 31), number_confirmed=40, number_total=40, number_today=40)]
     >>> lst_months = [1]
@@ -126,14 +130,16 @@ def cases_per_month_province(inputs: list[CasesData], months: list[int], years: 
     return dict_so_far
 
 
-def total_cases_per_years(inputs: list[CasesData], months: list[int], years: list[int], provinces: list[str]):
+def total_cases_per_years(inputs: list[CasesData], months: list[int], years: list[int],
+                          provinces: list[str]) -> dict[str, dict[tuple[int, int], int]]:
     """Return the Provinces mapped to the years mapped to the total cases per month.
-    
+
     Precondition:
         - month in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
         - year in [2020, 2021]
-        - province in ['Ontario', 'British Columbia', 'Alberta', 'Manitoba', 'Saskatchewan', 'Prince Edward Island',
-                     'New Brunswick', 'Nova Scotia', 'Quebec', 'Newfoundland and Labrador']
+        - province in ['Ontario', 'British Columbia', 'Alberta', 'Manitoba', 'Saskatchewan',
+        'Prince Edward Island', 'New Brunswick', 'Nova Scotia', 'Quebec',
+        'Newfoundland and Labrador']
     >>> method = [CasesData(province_id=35, province_name='Ontario', \
     date=datetime.date(2020, 1, 31), number_confirmed=40, number_total=40, number_today=40), \
     CasesData(province_id=35, province_name='Alberta', date=datetime.date(2020, 2, 10), \
@@ -150,3 +156,20 @@ def total_cases_per_years(inputs: list[CasesData], months: list[int], years: lis
         a = cases_per_month_province(inputs, months, years, province)
         province_cases_so_far[province] = a
     return province_cases_so_far
+
+
+if __name__ == '__main__':
+    # When you are ready to check your work with python_ta, uncomment the following lines.
+    # (Delete the "#" and space before each line.)
+    # IMPORTANT: keep this code indented inside the "if __name__ == '__main__'" block
+    # Leave this code uncommented when you submit your files.
+    import python_ta
+    import doctest
+
+    python_ta.check_all(config={
+        'extra-imports': ['python_ta.contracts', 'csv', 'datetime', 'dataclass'],
+        'allowed-io': [],
+        'max-line-length': 100,
+        'disable': ['R1705', 'C0200', 'R1710', 'E9998']
+    })
+    doctest.testmod()

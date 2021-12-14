8e22c3e16460e3b4311437110dc1e48a7458a8e4
Arthur0033 <95937909+Arthur0033@users.noreply.github.com>
2021-12-13 19:15:18 -0500

    Update covid19_cases.py

--- covid19_cases.py
+++ covid19_cases.py
@@ -28,7 +28,7 @@ class CasesData:
     Representation Invariants:
         - province_id in [35, 59, 24, 48, 43, 17, 61, 62, 12, 60, 99, 46, 47, 11]
         - province_name in ['Alberta', 'Ontario', 'Quebec', 'British Columbia', 'Manitoba', \
-                            'New Bruswick', 'Saskatchewan', 'Prince Edward Island', \
+                            'New Brunswick', 'Saskatchewan', 'Prince Edward Island', \
                             'Newfoundland and Labrador', 'Nova Scotia']
         - data.year in [2020, 2021]
         - date.month in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]

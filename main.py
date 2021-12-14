0e6b62cd2863435919135ab392f93a3668a56e89
JoAndNight <95553695+JoAndNight@users.noreply.github.com>
2021-12-14 00:14:54 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,17 +2,16 @@
 
 Copyright and Usage Information
 ===============================
-
 This file is provided solely for the personal and private use of Lemeng Dai, Arthur Iliescu,
 Jiaxin Li, Maisarah Zulkefli. Arthur All forms of distribution of this code,
 whether as given or with any changes, are expressly prohibited.
-
 This file is Copyright (c) 2021 Lemeng Dai, Arthur Iliescu, Jiaxin Li, Maisarah Zulkefli.
 """
 import prediction
 import visualization
 import pprint
 
+# You are expected to see the result in Python Console and your explorer
 if __name__ == '__main__':
     visualization.main()
     pprint.pprint(prediction.main())
